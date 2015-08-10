64f4ee139aef3dfecf176a366ff51158d8e9ca9f
osiastedian <osiastedian@gmail.com>
2015-08-10 23:58:32 +0800

    Added Update Functionality for TODO

--- BaseProject/src/project/controller/admin/todo/UpdateTodoController.java
+++ BaseProject/src/project/controller/admin/todo/UpdateTodoController.java
@@ -3,6 +3,9 @@ package project.controller.admin.todo;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 import org.slim3.datastore.Datastore;
+import org.slim3.repackaged.org.json.JSONObject;
+
+import com.google.appengine.api.datastore.Key;
 
 import project.dto.TodoDto;
 import project.model.TodoModel;
@@ -12,7 +15,7 @@ public class UpdateTodoController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
-        TodoService service = new TodoService();
+        /*TodoService service = new TodoService();
         TodoDto todo = new TodoDto();
         todo.setTitle("TitleTest0");
         todo.setKey(Datastore.createKey(TodoModel.class, todo.getTitle()));
@@ -22,6 +25,28 @@ public class UpdateTodoController extends Controller {
         todo.setId(123456);
         todo.setVersion((long)20);
         service.updateTodo(todo);
-        return this.redirect("/");
+        return this.redirect("/");*/
+        System.out.println((String)this.requestScope("data"));
+        TodoService service = new TodoService();
+        JSONObject json = new JSONObject();
+        TodoDto todo = new TodoDto();
+        
+        try {
+            json = new JSONObject((String)this.requestScope("data"));
+            todo.setTitle(json.getString("title"));
+            todo.setDescription(json.getString("description"));
+            todo.setFinished_quantity(json.getInt("finished_quantity"));
+            todo.setKey(Datastore.createKey(TodoModel.class, todo.getTitle()));
+            todo.setId(json.getInt("id"));
+            todo.setTotal_quantity(json.getInt("total_quantity"));
+            service.updateTodo(todo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            todo.getErrorList().add("Server controller error: " + e.getMessage());
+        }
+        json.put("errorList", todo.getErrorList());
+        response.setContentType("application/json");
+        response.getWriter().write(json.toString());
+        return null;
     }
 }
